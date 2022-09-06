809616e51fb3c199792c31c95dc20371707abc9e
Ramil Novruzov <ramil.novruzov@adesso.de>
2022-09-06 12:04:18 +0200

    xml-special-symbols decode-function added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import XMLParser from 'react-xml-parser';
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const specialSymbolXmlMap = {
+    '&#8211;': '-',
+    '%20': ' ',
+  };
+
+  const decodeXml = (string) => {
+    return string.replace(/(&#8211;|%20)/g, (item) => {
+      return specialSymbolXmlMap[item];
+    });
+  };
 
   useEffect(() => {
     fetch('https://dev98.de/feed/')
@@ -20,11 +30,10 @@ const App = () => {
       {items.map((item, index) => {
         return (
           <article>
-            <a
-              href={item.getElementsByTagName('link')[0].value}
-              key={item.getElementsByTagName('link')[0].value}
-            >
-              <h2 key={index}>{item.getElementsByTagName('title')[0].value}</h2>
+            <a href={item.getElementsByTagName('link')[0].value} key={index}>
+              <h2 key={index}>
+                {decodeXml(item.getElementsByTagName('title')[0].value)}
+              </h2>
             </a>
           </article>
         );
