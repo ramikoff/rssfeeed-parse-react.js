cbedc5253987434741af7a2c9d426670d582c7e1
Ramil Novruzov <ramil.novruzov@adesso.de>
2022-09-06 16:05:06 +0200

    description  + ConvertStringToHTML added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import XMLParser from 'react-xml-parser';
 
 const App = () => {
   const [items, setItems] = useState([]);
+
+  const ConvertStringToHTML = (str) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(str, 'text/html');
+    return doc.body.innerText;
+  };
+
   const specialSymbolXmlMap = {
     '&#8211;': '-',
     '%20': ' ',
+    ' >': '',
   };
 
   const decodeXml = (string) => {
-    return string.replace(/(&#8211;|%20)/g, (item) => {
+    return string.replace(/(&#8211;|%20| >)/g, (item) => {
       return specialSymbolXmlMap[item];
     });
   };
@@ -35,6 +43,12 @@ const App = () => {
                 {decodeXml(item.getElementsByTagName('title')[0].value)}
               </h2>
             </a>
+
+            <p>
+              {ConvertStringToHTML(
+                decodeXml(item.getElementsByTagName('description')[0].value)
+              )}
+            </p>
           </article>
         );
       })}
